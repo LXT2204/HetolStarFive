27a4b0515d3b000e20d4921ce70c1b3e7f170afa
Le Xuan Thach <21521421@gm.uit.edu.vn>
2023-10-23 11:58:23 +0700

    Category_room_add

--- app/Http/Controllers/CategoryRoom.php
+++ app/Http/Controllers/CategoryRoom.php
@@ -2,27 +2,87 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Slider;
+use App\Exports\ExcelExports;
+use App\Imports\ExcelImports;
+use Excel;
+use CategoryRoomModel;
 use Illuminate\Support\Facades\Session;
-use App\Models\Social; // Assuming your model namespace is App\Models
-use Socialite;
-use App\Models\Login; // Assuming your model namespace is App\Models
-use App\Http\Requests; // Make sure you have the appropriate namespace for Requests
+use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
-use Validator;
-use App\Http\Controllers\Captcha; // Assuming your Captcha rule is in the App\Rules namespace
 
+session_start();
 
 class CategoryRoom extends Controller
 {
+
     public function add_category_room()
     {
+
         return view('admin.add_category_room');
     }
     public function all_category_room()
     {
-        return view('admin.all_category_room');
+        $all_category_room = DB::table('tbl_category_room')->get();
+        $manager_category_room  = view('admin.all_category_room')->with('all_category_room', $all_category_room);
+        return view('admin_layout')->with('admin.all_category_room', $manager_category_room);
     }
+    public function save_category_room(Request $request)
+    {
+
+        $data = array();
+
+        $data['category_name'] = $request->category_room_name;
+        $data['category_desc'] = $request->category_room_desc;
+        $data['category_status'] = $request->category_room_status;
+
+        DB::table('tbl_category_room')->insert($data);
+        Session::put('message', 'Thêm loại phòng thành công');
+        return Redirect::to('add-category-room');
+    }
+    public function unactive_category_room($category_room_id)
+    {
+
+        DB::table('tbl_category_room')->where('category_id', $category_room_id)->update(['category_status' => 1]);
+        Session::put('message', 'Không kích hoạt danh mục sản phẩm thành công');
+        return Redirect::to('all-category-room');
+    }
+    public function active_category_room($category_room_id)
+    {
+
+        DB::table('tbl_category_room')->where('category_id', $category_room_id)->update(['category_status' => 0]);
+        Session::put('message', 'Kích hoạt danh mục sản phẩm thành công');
+        return Redirect::to('all-category-room');
+    }
+    public function edit_category_room($category_room_id)
+    {
+        $edit_category_room = DB::table('tbl_category_room')->where('category_id', $category_room_id)->get();
+
+        $manager_category_room  = view('admin.edit_category_room')->with('edit_category_room', $edit_category_room);
+
+        return view('admin_layout')->with('admin.edit_category_room', $manager_category_room);
+    }
+    public function update_category_room(Request $request, $category_room_id)
+    {
+        $data = array();
+        $data['category_name'] = $request->category_room_name;
+        $data['meta_keywords'] = $request->category_room_keywords;
+        $data['slug_category_room'] = $request->slug_category_room;
+        $data['category_desc'] = $request->category_room_desc;
+        DB::table('tbl_category_room')->where('category_id', $category_room_id)->update($data);
+        Session::put('message', 'Cập nhật danh mục sản phẩm thành công');
+        return Redirect::to('all-category-room');
+    }
+    public function delete_category_room($category_room_id)
+    {
+        DB::table('tbl_category_room')->where('category_id', $category_room_id)->delete();
+        Session::put('message', 'Xóa danh mục sản phẩm thành công');
+        return Redirect::to('all-category-room');
+    }
+
+
+    //End Function Admin Page
+
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,124 @@ Route::get('/add-category-room', 'App\Http\Controllers\CategoryRoom@add_category
 Route::get('/edit-category-room/{category-room-id}', 'App\Http\Controllers\CategoryRoom@edit_category_room');
 Route::get('/delete-category-room/{category-room-id}', 'App\Http\Controllers\CategoryRoom@delete_category_room');
 Route::get('/all-category-room', 'App\Http\Controllers\CategoryRoom@all_category_room');
+
+
+Route::post('/export-csv', 'App\Http\Controllers\CategoryRoom@export_csv');
+Route::post('/import-csv', 'App\Http\Controllers\CategoryRoom@import_csv');
+
+
+Route::get('/unactive-category-room/{category_room_id}', 'App\Http\Controllers\CategoryRoom@unactive_category_room');
+Route::get('/active-category-room/{category_room_id}', 'App\Http\Controllers\CategoryRoom@active_category_room');
+
+//Send Mail 
+Route::get('/send-mail', 'HomeController@send_mail');
+
+//Login facebook
+Route::get('/login-facebook', 'AdminController@login_facebook');
+Route::get('/admin/callback', 'AdminController@callback_facebook');
+
+//Login google
+Route::get('/login-google', 'AdminController@login_google');
+Route::get('/google/callback', 'AdminController@callback_google');
+
+Route::post('/save-category-room', 'App\Http\Controllers\CategoryRoom@save_category_room');
+Route::post('/update-category-room/{category_room_id}', 'App\Http\Controllers\CategoryRoom@update_category_room');
+
+//Brand room
+Route::get('/add-brand-room', 'Brandroom@add_brand_room');
+Route::get('/edit-brand-room/{brand_room_id}', 'Brandroom@edit_brand_room');
+Route::get('/delete-brand-room/{brand_room_id}', 'Brandroom@delete_brand_room');
+Route::get('/all-brand-room', 'Brandroom@all_brand_room');
+
+Route::get('/unactive-brand-room/{brand_room_id}', 'Brandroom@unactive_brand_room');
+Route::get('/active-brand-room/{brand_room_id}', 'Brandroom@active_brand_room');
+
+Route::post('/save-brand-room', 'Brandroom@save_brand_room');
+Route::post('/update-brand-room/{brand_room_id}', 'Brandroom@update_brand_room');
+
+
+//room
+// Route::group(['middleware' => 'roles', 'roles'=>['admin','author']], function () {
+Route::get('/add-room', 'roomController@add_room');
+Route::get('/edit-room/{room_id}', 'roomController@edit_room');
+// });
+Route::get(
+    'users',
+    [
+        'uses' => 'UserController@index',
+        'as' => 'Users',
+        'middleware' => 'roles'
+        // 'roles' => ['admin','author']
+    ]
+);
+Route::get('add-users', 'UserController@add_users');
+Route::post('store-users', 'UserController@store_users');
+Route::post('assign-roles', 'UserController@assign_roles');
+
+
+
+Route::get('/delete-room/{room_id}', 'roomController@delete_room');
+Route::get('/all-room', 'roomController@all_room');
+Route::get('/unactive-room/{room_id}', 'roomController@unactive_room');
+Route::get('/active-room/{room_id}', 'roomController@active_room');
+Route::post('/save-room', 'roomController@save_room');
+Route::post('/update-room/{room_id}', 'roomController@update_room');
+
+//Coupon
+Route::post('/check-coupon', 'CartController@check_coupon');
+
+Route::get('/unset-coupon', 'CouponController@unset_coupon');
+Route::get('/insert-coupon', 'CouponController@insert_coupon');
+Route::get('/delete-coupon/{coupon_id}', 'CouponController@delete_coupon');
+Route::get('/list-coupon', 'CouponController@list_coupon');
+Route::post('/insert-coupon-code', 'CouponController@insert_coupon_code');
+
+//Cart
+Route::post('/update-cart-quantity', 'CartController@update_cart_quantity');
+Route::post('/update-cart', 'CartController@update_cart');
+Route::post('/save-cart', 'CartController@save_cart');
+Route::post('/add-cart-ajax', 'CartController@add_cart_ajax');
+Route::get('/show-cart', 'CartController@show_cart');
+Route::get('/gio-hang', 'CartController@gio_hang');
+Route::get('/delete-to-cart/{rowId}', 'CartController@delete_to_cart');
+Route::get('/del-room/{session_id}', 'CartController@delete_room');
+Route::get('/del-all-room', 'CartController@delete_all_room');
+
+//Checkout
+Route::get('/dang-nhap', 'CheckoutController@login_checkout');
+Route::get('/del-fee', 'CheckoutController@del_fee');
+
+Route::get('/logout-checkout', 'CheckoutController@logout_checkout');
+Route::post('/add-customer', 'CheckoutController@add_customer');
+Route::post('/order-place', 'CheckoutController@order_place');
+Route::post('/login-customer', 'CheckoutController@login_customer');
+Route::get('/checkout', 'CheckoutController@checkout');
+Route::get('/payment', 'CheckoutController@payment');
+Route::post('/save-checkout-customer', 'CheckoutController@save_checkout_customer');
+Route::post('/calculate-fee', 'CheckoutController@calculate_fee');
+Route::post('/select-delivery-home', 'CheckoutController@select_delivery_home');
+Route::post('/confirm-order', 'CheckoutController@confirm_order');
+
+//Order
+Route::get('/delete-order/{order_code}', 'OrderController@order_code');
+Route::get('/print-order/{checkout_code}', 'OrderController@print_order');
+Route::get('/manage-order', 'OrderController@manage_order');
+Route::get('/view-order/{order_code}', 'OrderController@view_order');
+Route::post('/update-order-qty', 'OrderController@update_order_qty');
+Route::post('/update-qty', 'OrderController@update_qty');
+
+
+//Delivery
+Route::get('/delivery', 'DeliveryController@delivery');
+Route::post('/select-delivery', 'DeliveryController@select_delivery');
+Route::post('/insert-delivery', 'DeliveryController@insert_delivery');
+Route::post('/select-feeship', 'DeliveryController@select_feeship');
+Route::post('/update-delivery', 'DeliveryController@update_delivery');
+
+//Banner
+Route::get('/manage-slider', 'SliderController@manage_slider');
+Route::get('/add-slider', 'SliderController@add_slider');
+Route::get('/delete-slide/{slide_id}', 'SliderController@delete_slide');
+Route::post('/insert-slider', 'SliderController@insert_slider');
+Route::get('/unactive-slide/{slide_id}', 'SliderController@unactive_slide');
+Route::get('/active-slide/{slide_id}', 'SliderController@active_slide');
